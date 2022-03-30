23b86b31dbd01dd2fe07e7643a475328ffb7f22b
tarcisiozf <tar-2008.ferraz@hotmail.com>
2022-03-30 17:22:02 -0300

    wip update by tag

--- bin.js
+++ bin.js
@@ -1,11 +1,14 @@
 #!/usr/bin/env node
 const checkUnstableDependencies = require('./src/check-unstable-dependencies')
+const updateByTag = require('./src/update-by-tag')
 
 const [, , command, ...args] = process.argv
 
 switch (command) {
   case 'check-unstable-deps':
     return checkUnstableDependencies(...args)
+  case 'update-by-tag':
+    return updateByTag(...args)
   default: {
     console.error('ERROR: invalid command')
     process.exit(1)
